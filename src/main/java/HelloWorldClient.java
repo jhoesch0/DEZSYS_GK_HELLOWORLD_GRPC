22070d78e2773822e03073e29fcde0492329ce40
jhoesch <jhoesch@student.tgm.ac.at>
2025-12-02 13:21:43 +0100

    Gk done

--- src/main/java/HelloWorldClient.java
+++ src/main/java/HelloWorldClient.java
@@ -5,8 +5,8 @@ public class HelloWorldClient {
 
     public static void main(String[] args) {
 
-        String firstname = args.length > 0 ? args[0] : "Max";
-        String lastname  = args.length > 1 ? args[1] : "Mustermann";
+        String firstname = args.length > 0 ? args[0] : "Jonas";
+        String lastname = args.length > 1 ? args[1] : "Hösch";
 
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext()
@@ -18,9 +18,39 @@ public class HelloWorldClient {
                 .setFirstname(firstname)
                 .setLastname(lastname)
                 .build());
-        System.out.println( helloResponse.getText() );
-        channel.shutdown();
+        System.out.println(helloResponse.getText());
+
+
+
+        System.out.println("\nSending WarehouseRecord...\n");
+
+        // Build product list
+        Hello.ProductData p1 = Hello.ProductData.newBuilder()
+                .setProductId(101)
+                .setProductName("Laptop Pro 15")
+                .setPrice(1499.99)
+                .setQuantity(3)
+                .build();
+
+        Hello.ProductData p2 = Hello.ProductData.newBuilder()
+                .setProductId(102)
+                .setProductName("Mouse Wireless")
+                .setPrice(29.99)
+                .setQuantity(10)
+                .build();
+
+        // Build warehouse record
+        Hello.WarehouseRecord warehouse = Hello.WarehouseRecord.newBuilder()
+                .setWarehouseID("W-001")
+                .setWarehouseName("Main Warehouse Europe")
+                .setTimestamp("2025-12-02T10:15:00Z")
+                .addProductDataList(p1)
+                .addProductDataList(p2)
+                .build();
+
+        // Call RPC
+        Hello.HelloResponse warehouseResponse = stub.sendWarehouse(warehouse);
+        System.out.println("Server Response: " + warehouseResponse.getText());
 
     }
-
 }
--- src/main/java/HelloWorldServiceImpl.java
+++ src/main/java/HelloWorldServiceImpl.java
@@ -3,7 +3,7 @@ import io.grpc.stub.StreamObserver;
 public class HelloWorldServiceImpl extends HelloWorldServiceGrpc.HelloWorldServiceImplBase {
 
     @Override
-    public void hello( Hello.HelloRequest request, StreamObserver<Hello.HelloResponse> responseObserver) {
+    public void hello(Hello.HelloRequest request, StreamObserver<Hello.HelloResponse> responseObserver) {
 
         System.out.println("Handling hello endpoint: " + request.toString());
 
@@ -14,4 +14,34 @@ public class HelloWorldServiceImpl extends HelloWorldServiceGrpc.HelloWorldServi
         responseObserver.onCompleted();
 
     }
+
+    @Override
+    public void sendWarehouse(Hello.WarehouseRecord request,
+                              StreamObserver<Hello.HelloResponse> responseObserver) {
+
+        System.out.println("Handling sendWarehouse endpoint: ");
+        System.out.println("Warehouse ID: " + request.getWarehouseID());
+        System.out.println("Warehouse Name: " + request.getWarehouseName());
+        System.out.println("Timestamp: " + request.getTimestamp());
+        System.out.println("Products:");
+
+        for (Hello.ProductData p : request.getProductDataListList()) {
+            System.out.println("- " + p.getProductName() +
+                    " (ID " + p.getProductId() +
+                    ", Price " + p.getPrice() +
+                    ", Qty " + p.getQuantity() + ")");
+        }
+
+        String summary = "Warehouse '" + request.getWarehouseName()
+                + "' received with " + request.getProductDataListCount()
+                + " products.";
+
+        Hello.HelloResponse response = Hello.HelloResponse.newBuilder()
+                .setText(summary)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
 }
